47aeb23b9ddbcfd934a8be332d78b29b98d2a80f
alsak0de <63293958+alsak0de@users.noreply.github.com>
2023-01-18 10:17:55 +0100

    Create wa-scraper.py

---
+++ src/wa-scraper/wa-scraper.py
@@ -0,0 +1,52 @@
+import requests
+
+# refustered function returns a boolean value
+def WSP_registered(phone, apikey):
+    url = "https://whatsapp-scraper.p.rapidapi.com/wchk"        
+    querystring = {"phone":phone}
+    headers = {
+	    "X-RapidAPI-Key": apikey,
+	    "X-RapidAPI-Host": "whatsapp-scraper.p.rapidapi.com"
+    }
+    return bool(requests.request("GET", url, headers=headers, params=querystring).json())
+
+# about function returns a json object
+def WSP_about(phone, apikey):
+    url = "https://whatsapp-scraper.p.rapidapi.com/about"        
+    querystring = {"phone":phone}
+    headers = {
+	    "X-RapidAPI-Key": apikey,
+	    "X-RapidAPI-Host": "whatsapp-scraper.p.rapidapi.com"
+    }
+    return requests.request("GET", url, headers=headers, params=querystring).json()
+
+# Isbiz returns a boolean value
+def WSP_isbiz(phone, apikey):
+    url = "https://whatsapp-scraper.p.rapidapi.com/isbiz"        
+    querystring = {"phone":phone}
+    headers = {
+	    "X-RapidAPI-Key": apikey,
+	    "X-RapidAPI-Host": "whatsapp-scraper.p.rapidapi.com"
+    }
+    return bool(requests.request("GET", url, headers=headers, params=querystring).json())
+
+# Bizinfo returns a json object
+def WSP_bizinfo(phone, apikey):
+    url = "https://whatsapp-scraper.p.rapidapi.com/bizinfo"        
+    querystring = {"phone":phone}
+    headers = {
+	    "X-RapidAPI-Key": apikey,
+	    "X-RapidAPI-Host": "whatsapp-scraper.p.rapidapi.com"
+    }
+    return requests.request("GET", url, headers=headers, params=querystring).json()
+
+# Valid values for pictype are 'url', 'base64' and 'png'.
+# Valid values for quality are 'high' and 'low'
+def WSP_profilepic(phone, apikey,pictype="url", quality="high"):
+    url = "https://whatsapp-scraper.p.rapidapi.com/wspicture"        
+    querystring = {"phone":phone,"pictype":pictype,"quality":quality}
+    headers = {
+	    "X-RapidAPI-Key": apikey,
+	    "X-RapidAPI-Host": "whatsapp-scraper.p.rapidapi.com"
+    }
+    return requests.request("GET", url, headers=headers, params=querystring)
