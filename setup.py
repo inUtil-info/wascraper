8cbe574a32d3e0ca949bd6aede519ab24a8e449f
alsak0de <63293958+alsak0de@users.noreply.github.com>
2023-01-18 21:05:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,2 @@
-from setuptools import setup
-  
-setup(
-    name='wa_scraper',
-    version='0.1',
-    description='python functions to access whatsapp scraper',
-    author='inutil labs',
-    author_email='info@inutil.info',
-    packages=['wa_scraper'],
-    install_requires=[
-        'requests',
-    ],
-)
+from setuptools import setupif __name__ == '__main__':
+ setup()
